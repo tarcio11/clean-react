96f6683c9fd5a35337bbab947651632232be4655
Tarcio Rocha <tarcio_x@hotmail.com>
2021-01-19 13:15:23 -0300

    chore: add files to coverage ignore

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Router } from 'react-router-dom'
 import { createMemoryHistory } from 'history'
-import Login from './login'
+import { Login } from '@/presentation/pages'
 import faker from 'faker'
 import 'jest-localstorage-mock'
 import { render, RenderResult, fireEvent, cleanup, waitFor } from '@testing-library/react'
