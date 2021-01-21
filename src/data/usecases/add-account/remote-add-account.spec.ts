ea74e86145a2649a443103a592011a9a92778da0
Tarcio Rocha <tarcio_x@hotmail.com>
2021-01-21 05:52:31 -0300

    test: ensure RemoteAddAccount returns an AccountModel on sucess

--- src/data/usecases/add-account/remote-add-account.spec.ts
+++ src/data/usecases/add-account/remote-add-account.spec.ts
@@ -4,7 +4,7 @@ import { HttpStatusCode } from '@/data/protocols/http'
 import { AccountModel } from '@/domain/models'
 import { AddAccountParams } from '@/domain/usecases'
 import { EmailInUseError, UnexpectedError } from '@/domain/errors'
-import { mockAddAccountParams } from '@/domain/test'
+import { mockAccountModel, mockAddAccountParams } from '@/domain/test'
 import faker from 'faker'
 
 type SutTypes = {
@@ -70,5 +70,16 @@ describe('RemoteAddAccount', () => {
     }
     const promise = sut.add(mockAddAccountParams())
     await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
+
+  test('should return an AccountModel if HttpPostClient returns 200', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    const httpResult = mockAccountModel()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.ok,
+      body: httpResult
+    }
+    const account = await sut.add(mockAddAccountParams())
+    expect(account).toEqual(httpResult)
   })
 })
