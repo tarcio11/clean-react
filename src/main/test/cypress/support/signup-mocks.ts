e59a396593870a14e2a9f167013c0d53d675e714
Tarcio Rocha <tarcio_x@hotmail.com>
2021-01-25 10:50:10 -0300

    test: ensure SignUp save accessToken on localstorage on success

--- src/main/test/cypress/support/signup-mocks.ts
+++ src/main/test/cypress/support/signup-mocks.ts
@@ -4,3 +4,4 @@ import faker from 'faker'
 export const mockEmailInUseError = (): void => Helper.mockEmailInUseError(/signup/)
 export const mockUnexpectedErrorError = (): void => Helper.mockUnexpectedErrorError(/signup/, 'POST')
 export const mockInvalidData = (): void => Helper.mockOk(/signup/, 'POST', { invalidProperty: faker.random.uuid() })
+export const mockOk = (): void => Helper.mockOk(/signup/, 'POST', { accessToken: faker.random.uuid() })
