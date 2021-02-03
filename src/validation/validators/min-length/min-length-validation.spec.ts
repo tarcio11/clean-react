b4e93fab36b05273ea01cd3815729309e20132c3
Tarcio Rocha <tarcio_x@hotmail.com>
2021-02-03 18:15:13 -0300

    refactor: remove unused file

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -1,27 +1,34 @@
-import { MinLengthValidation } from './min-length-validation'
+import { MinLengthValidation } from '@/validation/validators'
 import { InvalidFieldError } from '@/validation/errors'
+
 import faker from 'faker'
 
 const makeSut = (field: string): MinLengthValidation => new MinLengthValidation(field, 5)
 
 describe('MinLengthValidation', () => {
-  test('should return error if value is invalid', () => {
+  test('Should return error if value is invalid', () => {
     const field = faker.database.column()
     const sut = makeSut(field)
+
     const error = sut.validate({ [field]: faker.random.alphaNumeric(4) })
+
     expect(error).toEqual(new InvalidFieldError())
   })
 
-  test('should return falsy if value is valid', () => {
+  test('Should return falsy if value is valid', () => {
     const field = faker.database.column()
     const sut = makeSut(field)
+
     const error = sut.validate({ [field]: faker.random.alphaNumeric(5) })
+
     expect(error).toBeFalsy()
   })
 
-  test('should return falsy if field does not exist in schema', () => {
-    const sut = makeSut(faker.database.column())
-    const error = sut.validate({ [faker.database.column()]: faker.random.alphaNumeric(5) })
+  test('Should return falsy if field does not exists in schema', () => {
+    const sut = makeSut('any_field')
+
+    const error = sut.validate({ invalidField: faker.random.alphaNumeric(5) })
+
     expect(error).toBeFalsy()
   })
 })
