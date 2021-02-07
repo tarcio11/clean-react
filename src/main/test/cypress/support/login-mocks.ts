209808d174ef0acd1b8be48c6f8860bd562f4c91
Tarcio Rocha <tarcio_x@hotmail.com>
2021-02-07 07:56:08 -0300

    refactor: improve integration test helper names

--- src/main/test/cypress/support/login-mocks.ts
+++ src/main/test/cypress/support/login-mocks.ts
@@ -1,7 +1,6 @@
-import * as Helper from './http-mocks'
+import * as Http from './http-mocks'
 import faker from 'faker'
 
-export const mockInvalidCredentialsError = (): void => Helper.mockInvalidCredentialsError(/login/)
-export const mockUnexpectedErrorError = (): void => Helper.mockUnexpectedErrorError(/login/, 'POST')
-export const mockOk = (): void => Helper.mockOk(/login/, 'POST', { accessToken: faker.random.uuid(), name: faker.name.findName() })
-export const mockInvalidData = (): void => Helper.mockOk(/login/, 'POST', { invalidProperty: faker.random.uuid() })
+export const mockInvalidCredentialsError = (): void => Http.mockUnauthorizedError(/login/)
+export const mockUnexpectedErrorError = (): void => Http.mockServerErrorError(/login/, 'POST')
+export const mockOk = (): void => Http.mockOk(/login/, 'POST', { accessToken: faker.random.uuid(), name: faker.name.findName() })
--- src/main/test/cypress/support/signup-mocks.ts
+++ src/main/test/cypress/support/signup-mocks.ts
@@ -1,7 +1,6 @@
-import * as Helper from './http-mocks'
+import * as Http from './http-mocks'
 import faker from 'faker'
 
-export const mockEmailInUseError = (): void => Helper.mockEmailInUseError(/signup/)
-export const mockUnexpectedErrorError = (): void => Helper.mockUnexpectedErrorError(/signup/, 'POST')
-export const mockOk = (): void => Helper.mockOk(/signup/, 'POST', { accessToken: faker.random.uuid(), name: faker.name.findName() })
-export const mockInvalidData = (): void => Helper.mockOk(/signup/, 'POST', { invalidProperty: faker.random.uuid() })
+export const mockEmailInUseError = (): void => Http.mockForbiddenError(/signup/, 'POST')
+export const mockUnexpectedErrorError = (): void => Http.mockServerErrorError(/signup/, 'POST')
+export const mockOk = (): void => Http.mockOk(/signup/, 'POST', { accessToken: faker.random.uuid(), name: faker.name.findName() })
