2356b4604bfe1734a7a4fe983a1c5f2762181ee2
Tarcio Rocha <tarcio_x@hotmail.com>
2021-02-08 10:27:14 -0300

    chore: add webpack prod and dev configs

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,21 +1,10 @@
-const path = require('path')
 const { DefinePlugin } = require('webpack')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const common = require('./webpack.common')
+const { merge } = require('webpack-merge')
 
-module.exports = {
+module.exports = merge(common, {
   mode: 'development',
-  entry: './src/main/index.tsx',
-  output: {
-    path: path.join(__dirname, 'public/js'),
-    publicPath: '/public/js',
-    filename: 'bundle.js'
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', 'scss'],
-    alias: {
-      '@': path.join(__dirname, 'src')
-    }
-  },
   module: {
     rules: [{
       test: /\.ts(x?)$/,
@@ -43,14 +32,12 @@ module.exports = {
     writeToDisk: true,
     historyApiFallback: true
   },
-  externals: {
-    react: 'React',
-    'react-dom': 'ReactDOM'
-  },
   plugins: [
-    new CleanWebpackPlugin(),
     new DefinePlugin({
       'process.env.API_URL': JSON.stringify('https://clean-nodejs-api-dev.herokuapp.com/api')
+    }),
+    new HtmlWebPackPlugin({
+      template: './template.dev.html'
     })
   ]
-}
+})
